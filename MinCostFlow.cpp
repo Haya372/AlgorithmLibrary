fd2f2dba6133105b49b62745e5610b82617069f0
Hayato Mitomi <mitomihayato@mitomihayatonoMacBook-Pro.local>
2021-12-30 18:16:44 +0900

    [fix] convert MinCostFlow algorithm to class

--- MinCostFlow.cpp
+++ MinCostFlow.cpp
@@ -4,70 +4,71 @@ using namespace std;
 
 typedef pair<int, int> P;
 
-const int INF = 1e8;
+class MinCostFlow{
+  struct edge { int to, cap, cost, rev; };
+  vector<vector<edge>> G;
+  const int INF = 1e8;
+  vector<int> h; // ポテンシャル＝残余グラフにおけるスタートからの最短経路
+  vector<int> dist; // 最短距離（ダイクストラ法に用いる)
+  vector<int> prevv, preve; // 直前の頂点と辺
+  int V;
 
-struct edge { int to, cap, cost, rev; };
+  public:
+  MinCostFlow(int n){
+    V = n;
+    G.resize(n);
+    prevv.resize(n);
+    preve.resize(n);
+  }
 
-int V; // 頂点数
-// 以下の変数はVの値が確定後、reshape(V)をしておく
-vector<int> h; // ポテンシャル＝残余グラフにおけるスタートからの最短経路
-vector<int> dist; // 最短距離（ダイクストラ法に用いる)
-vector<int> prevv, preve; // 直前の頂点と辺
-vector<vector<edge>> G;
+  void add_edge(int from, int to, int cap, int cost){
+    G[from].push_back((edge){ to, cap, cost, (int)G[to].size() });
+    G[to].push_back((edge){ from, 0, -cost, (int)G[from].size() - 1 });
+  }
 
-void add_edge(int from, int to, int cap, int cost){
-  G[from].push_back((edge){ to, cap, cost, (int)G[to].size() });
-  G[to].push_back((edge){ from, 0, -cost, (int)G[from].size() - 1 });
-}
-
-// s: 始点ノード, t: 終点ノード, f: 流したい流量
-int min_cost_flow(int s, int t, int f){
-  int res = 0;
-  h = vector<int> (V, 0);
-  while(f > 0){
-    priority_queue<P, vector<P>, greater<P>> que;
-    dist = vector<int> (V, INF);
-    dist[s] = 0;
-    que.push(make_pair(0, s));
-    while(!que.empty()){
-      P now = que.top(); que.pop();
-      int v = now.second;
-      int cost = now.first;
-      if(dist[v] < cost) continue;
-      for(int i = 0; i < G[v].size(); i++){
-        edge &e = G[v][i];
-        if(e.cap > 0 && dist[e.to] > dist[v] + e.cost + h[v] - h[e.to]){
-          dist[e.to] = dist[v] + e.cost + h[v] - h[e.to];
-          prevv[e.to] = v;
-          preve[e.to] = i;
-          que.push(make_pair(dist[e.to], e.to));
+  // s: 始点ノード, t: 終点ノード, f: 流したい流量
+  int min_cost_flow(int s, int t, int f){
+    int res = 0;
+    h = vector<int> (V, 0);
+    while(f > 0){
+      priority_queue<P, vector<P>, greater<P>> que;
+      dist = vector<int> (V, INF);
+      dist[s] = 0;
+      que.push(make_pair(0, s));
+      while(!que.empty()){
+        P now = que.top(); que.pop();
+        int v = now.second;
+        int cost = now.first;
+        if(dist[v] < cost) continue;
+        for(int i = 0; i < G[v].size(); i++){
+          edge &e = G[v][i];
+          if(e.cap > 0 && dist[e.to] > dist[v] + e.cost + h[v] - h[e.to]){
+            dist[e.to] = dist[v] + e.cost + h[v] - h[e.to];
+            prevv[e.to] = v;
+            preve[e.to] = i;
+            que.push(make_pair(dist[e.to], e.to));
+          }
         }
       }
-    }
-    if(dist[t] == INF){
-      // これ以上流せない状態
-      return -1;
-    }
-    for(int i = 0; i < V; i ++) h[i] += dist[i];
+      if(dist[t] == INF){
+        // これ以上流せない状態
+        return -1;
+      }
+      for(int i = 0; i < V; i ++) h[i] += dist[i];
 
-    // 見つかった最短経路に流せるだけ流す
-    int d = f;
-    for(int v = t; v != s; v = prevv[v]){
-      d = min(d, G[prevv[v]][preve[v]].cap);
+      // 見つかった最短経路に流せるだけ流す
+      int d = f;
+      for(int v = t; v != s; v = prevv[v]){
+        d = min(d, G[prevv[v]][preve[v]].cap);
+      }
+      f -= d;
+      res += d * h[t];
+      for(int v = t; v != s; v = prevv[v]){
+        edge &e = G[prevv[v]][preve[v]];
+        e.cap -= d;
+        G[v][e.rev].cap += d;
+      }
     }
-    f -= d;
-    res += d * h[t];
-    for(int v = t; v != s; v = prevv[v]){
-      edge &e = G[prevv[v]][preve[v]];
-      e.cap -= d;
-      G[v][e.rev].cap += d;
-    }
+    return res;
   }
-  return res;
-}
-
-void init(){
-  G.resize(V);
-  prevv.resize(V);
-  preve.resize(V);
-}
+};
